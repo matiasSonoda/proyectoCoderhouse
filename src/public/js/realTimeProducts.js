9a68f63a887dbb496138e5ca6242495ba91f94c0
sonodetha <matiasnsonoda@gmail.com>
2023-09-20 21:55:14 -0300

    message commit

---
+++ src/public/js/realTimeProducts.js
@@ -0,0 +1,33 @@
+document.getElementById('formProduct').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de la forma predeterminada
+  
+    // Crea un objeto FormData a partir del formulario
+    var formData = new FormData(this);
+  
+    // Realiza una solicitud AJAX para agregar el producto
+    fetch('/api/products', {
+      method: 'POST',
+      body: formData
+    })
+    .then(function(response) {
+      return response.json(); // Convierte la respuesta en JSON
+    })
+    .then(function(product) {
+      // Agrega el nuevo producto a la lista de productos
+      var productsContainer = document.getElementById('products-container');
+      var productDiv = document.createElement('div');
+      productDiv.innerHTML = `
+        <p>ID:${product.id}</p>
+        <p>Titulo:${product.title}</p>
+        <p>Descripcion:${product.description}</p>
+        <p>Precio:${product.price}</p>
+        <p>status:${product.status}</p>
+        <p>code:${product.code}</p>
+        <p>stock:${product.stock}</p>
+      `;
+      productsContainer.appendChild(productDiv);
+    })
+    .catch(function(error) {
+      console.error('Error:', error);
+    });
+  });
