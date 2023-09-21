5bc7c4d0a6e7780587c04e7cf02aa202c34295b2
sonodetha <matiasnsonoda@gmail.com>
2023-09-20 23:15:59 -0300

    message commit

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,23 +6,15 @@ const usersRouter = Router()
 usersRouter.get("/signin",async(req,res)=>{
     res.render("signin")
   })
-/*usersRouter.post("/signin",async(req,res)=>{
+usersRouter.post("/signin",async(req,res)=>{
     const {first_name, last_name, email, password, age}= req.body
     try{
-        const response = await usersModel.create({first_name,last_name,email,age,password})
-        res.status(200).send({mensaje:"Usuario creado ",respuesta: response})
+        const newUser = await usersModel.create({first_name,last_name,email,age,password})
+        await newUser.save()
+        res.redirect('/static/login');
     }
     catch(error){
         res.status(400).send(error)
     }
-})*/
-usersRouter.post('/signin', async (req, res) => {
-    try {
-      const newUser = new User(req.body);
-      await newUser.save();
-      res.redirect('/static/login'); // Redirige al usuario a la página de login después del registro
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  })
+})
 export default usersRouter
